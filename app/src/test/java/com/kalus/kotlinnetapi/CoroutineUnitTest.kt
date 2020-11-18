2c31de811efeff95ec2f75e7db068871b404797a
biaowen.yu <yubw@edde.com.cn>
2020-11-18 18:38:43 +0800

    test(单元测试): 协程启动依赖库引入

--- app/src/test/java/com/kalus/kotlinnetapi/CoroutineUnitTest.kt
+++ app/src/test/java/com/kalus/kotlinnetapi/CoroutineUnitTest.kt
@@ -1,5 +1,7 @@
 package com.kalus.kotlinnetapi
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -9,9 +11,14 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class CoroutineUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testLaunch() {
+        GlobalScope.launch {
+            println("user info")
+        }
+        repeat(8){
+            println("main thread $it")
+        }
     }
 }
