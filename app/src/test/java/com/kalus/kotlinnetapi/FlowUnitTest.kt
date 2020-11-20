3c4e1f426f7b10ac57c17f55c4ed00e865479e3a
kalusyu <kalusyu@aliyun.com>
2020-11-20 09:32:43 +0800

    test(单元测试): kotlin flow 超时取消和构建器

--- app/src/test/java/com/kalus/kotlinnetapi/FlowUnitTest.kt
+++ app/src/test/java/com/kalus/kotlinnetapi/FlowUnitTest.kt
@@ -2,10 +2,12 @@ package com.kalus.kotlinnetapi
 
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeoutOrNull
 import org.junit.Test
 
 /**
@@ -32,10 +34,31 @@ class FlowUnitTest {
 
     }
 
-    fun simple(): Flow<Int> = flow {
+    private fun simple(): Flow<Int> = flow {
         for (i in 1..3){
             delay(100)
+            println("Emit $i")
             emit(i)
         }
     }
+
+    //超时取消Flow
+    @Test
+    fun testCancelFlow() = runBlocking {
+        withTimeoutOrNull(250L){
+            simple().collect {
+                println(it)
+            }
+        }
+        println("Done")
+    }
+
+    // 流构建器
+    @Test
+    fun testFlowConstruct() = runBlocking {
+        (1..3).asFlow().collect {
+            println("flow construct $it")
+        }
+    }
+
 }
